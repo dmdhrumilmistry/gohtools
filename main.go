02bb960644e67bb5418dbdec451e63ff62a5a12e
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-01-12 23:04:01 +0530

    refactor project files and code

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 func main() {
 	// StartEchoServer(4444, "ioCopyEcho")
 	// StartTcpProxy(4444, "localhost:8000")
-	StartListener(4444, HandleNcConn) // connect with server using net.Dial or nc IP 4444
+	// StartListener(4444, HandleNcConn) // connect with server using net.Dial or nc IP 4444
 }
