365d3c7dff27d53347fd7ee0aa1f7d8f41c6d24d
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-01-12 10:57:52 +0530

    create TCP proxy

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-	StartEchoServer(4444, "ioCopyEcho")
+	// StartEchoServer(4444, "ioCopyEcho")
+	StartTcpProxy(4444, "localhost:8000")
 }
---
+++ tcpProxy.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func HandleProxyRequest(src net.Conn, addr string) {
+	dst, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Printf("[!] Unable to connect to host: %s", addr)
+		return
+	}
+
+	defer dst.Close()
+
+	go func(dst net.Conn, src net.Conn, addr string) {
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Printf("[!] Error Occur while proxying data: %s\n", addr)
+		}
+	}(dst, src, addr)
+
+	if _, err := io.Copy(src, dst); err != nil {
+		log.Printf("[!] Error Occur while reverting data: %s\n", addr)
+	}
+}
+
+func StartTcpProxy(listenPort int, remoteAddr string) {
+	listenAddr := fmt.Sprintf("0.0.0.0:%d", listenPort)
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Printf("[!] Unable to start server due to error: %s\n", remoteAddr)
+	}
+	log.Printf("[*] Listening on %s", listenAddr)
+
+	for {
+		conn, err := listener.Accept()
+		log.Printf("[*] Connection from %s", conn.RemoteAddr().String())
+		if err != nil {
+			log.Printf("[!] Unable to connect to remote server due to error: %s\n", remoteAddr)
+		}
+
+		go HandleProxyRequest(conn, remoteAddr)
+	}
+}
